0ec15b16d022cb952ab57f88a8c6cd1b20be15e3
K-Basket <koshykkv@gmail.com>
2023-05-02 14:10:55 +0300

    err

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,18 +20,3 @@ export const App = () => {
     </>
   );
 };
-
-{
-  /* <div
-  style={{
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: 40,
-    color: '#010101',
-  }}
->
-  React homework by K-Basket
-</div>; */
-}
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './FriendListItem.module.css';
+// import css from './FriendListItem.module.css';
 
 export function FriendListItem({ avatar, name, isOnline }) {
   return (
